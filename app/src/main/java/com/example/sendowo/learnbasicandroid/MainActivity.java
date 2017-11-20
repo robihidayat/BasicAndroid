cb095141ddf747c34de1beba64244368d2b6d4ef
robi hidayat <robi.hidayat@mail.ugm.ac.id>
2017-11-20 22:55:37 +0700

    "Add Java"

--- app/src/main/java/com/example/sendowo/learnbasicandroid/MainActivity.java
+++ app/src/main/java/com/example/sendowo/learnbasicandroid/MainActivity.java
@@ -2,12 +2,40 @@ package com.example.sendowo.learnbasicandroid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btn1, btn2;
+    TextView txtView1, txtView2;
+    EditText editText1, editText2;
+//https://stackoverflow.com/questions/25905086/multiple-buttons-onclicklistener-android
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn1 = (Button) findViewById(R.id.button);
+        btn2 = (Button) findViewById(R.id.button2);
+        txtView1 = (TextView) findViewById(R.id.textView);
+        txtView2 = (TextView) findViewById(R.id.textView2);
+        editText1 = (EditText) findViewById(R.id.editText);
+        editText2 = (EditText) findViewById(R.id.editText2);
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                txtView1.setText(editText1.getText().toString());
+                txtView2.setText(editText2.getText().toString());
+            }
+        });
+
+
+
+
     }
 }
